ab07d620d379b4928d5f9e91e3434c36e36f805b
Oddlaug <47041203+Oddlaug@users.noreply.github.com>
2019-03-19 23:03:26 +0300

    Add files via upload

--- myprinter.py
+++ myprinter.py
@@ -2,29 +2,35 @@
 
 
 def adv_print(func):
-    def wrapper(s, **kwargs):
-        new_string = ''
+    def wrapper(*args, **kwargs):
+        s = ''.join([str(i) for i in args])
+        length = len(s)
+        str_start = '\n'
+        str_body = s
+
         if 'start' in kwargs:
-            start = kwargs['start'] + '\n'
-        else:
-            start = '\n'
+            str_start = kwargs['start']
 
-        if 'mas_length' in kwargs:
-            if len(s) > kwargs['mas_length']:
-                new_string += s[:kwargs['mas_length']] + '\n' + s[kwargs['mas_length']:]
-                new_string = start + new_string
+        if 'max_length' in kwargs:
+            if length > kwargs['max_length']:
+                str_body = s[:kwargs['max_length']] + '\n' + s[kwargs['max_length']:]
+            else:
+                str_body = s
 
-        if 'in_file' in kwargs:
+        new_string = str_start + str_body
+
+        if 'in_file' in kwargs and new_string:
             try:
                 with open(kwargs['in_file'], 'w', encoding='utf-8') as f:
                     f.write(new_string)
             except OSError:
                 print(f'Недопустимое имя файла => {kwargs["in_file"]}')
+
         return func(new_string)
 
     return wrapper
 
 
 @adv_print
-def printer(data):
-    return print(data)
+def printer(*args, **kwargs):
+    return print(*args, *kwargs)
